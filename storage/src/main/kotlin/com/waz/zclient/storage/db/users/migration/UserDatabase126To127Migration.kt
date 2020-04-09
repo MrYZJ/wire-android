7df196045a3636c9e0fc9bc220b7c8a4c5140fb7
Maciej Gorywoda <makingthematrix@gmail.com>
2020-04-09 16:39:41 +0200

    test fix: Let Kotlin create the Buttons table

--- storage/src/main/kotlin/com/waz/zclient/storage/db/users/migration/UserDatabase126To127Migration.kt
+++ storage/src/main/kotlin/com/waz/zclient/storage/db/users/migration/UserDatabase126To127Migration.kt
@@ -799,14 +799,15 @@ val USER_DATABASE_MIGRATION_126_TO_127 = object : Migration(126, 127) {
     }
 
     private fun createButtonsTable(database: SupportSQLiteDatabase) {
+        println("here too!")
         database.execSQL("""
-            CREATE TABLE IF NOT EXISTS `Buttons` (
-                `message_id` TEXT NOT NULL, 
-                `button_id` TEXT NOT NULL, 
-                `title` TEXT NOT NULL,
-                `ordinal` INTEGER NOT NULL,
-                `state` INTEGER NOT NULL, 
-                PRIMARY KEY(`message_id`, `button_id`)
+            CREATE TABLE IF NOT EXISTS Buttons (
+                message_id TEXT NOT NULL, 
+                button_id TEXT NOT NULL, 
+                title TEXT NOT NULL,
+                ordinal INTEGER NOT NULL,
+                state INTEGER NOT NULL, 
+                PRIMARY KEY (message_id, button_id)
             )""".trimIndent()
         )
     }
